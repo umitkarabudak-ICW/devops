cd08b1826045a36a53c5e66bdc083972d2fd195c
umitkarabudak-ICW <107492009+umitkarabudak-ICW@users.noreply.github.com>
2024-01-02 16:54:21 +0100

    rancher and traefik helm

--- redis-master/redis.py
+++ redis-master/redis.py
@@ -9,29 +9,28 @@ def write_and_verify_data(redis_client, user_id):
     # Veriyi Redis'e yaz
     redis_client.set(key, value_to_store)
     print(f"Kullanici {user_id}: Veri Redis'e başariyla eklendi. Anahtar: {key}, Değer: {value_to_store}")
-    
-    #3sn bekle.
-    
-    time.sleep(3)
-    # Veriyi Redis'den oku
-    retrieved_value = redis_client.get(key)
-    if retrieved_value:
-        retrieved_value = retrieved_value.decode('utf-8')
-        print(f"Kullanici {user_id}: Redis'ten alinan değer: {retrieved_value}")
 
-        # Veriyi doğrula
-        if retrieved_value == value_to_store:
-            print(f"Kullanici {user_id}: Veri doğrulandi.")
+    while True:
+        # Veriyi Redis'den oku
+        retrieved_value = redis_client.get(key)
+        if retrieved_value:
+            retrieved_value = retrieved_value.decode('utf-8')
+            print(f"Kullanici {user_id}: Redis'ten alinan değer: {retrieved_value}")
+
+            # Veriyi doğrula
+            if retrieved_value == value_to_store:
+                print(f"Kullanici {user_id}: Veri doğrulandi.")
+            else:
+                print(f"Kullanici {user_id}: Hata: Alinan veri, beklenen veri ile uyuşmuyor.")
         else:
-            print(f"Kullanici {user_id}: Hata: Alinan veri, beklenen veri ile uyuşmuyor.")
-    else:
-        print(f"Kullanici {user_id}: Hata: Belirtilen anahtar Redis veritabaninda bulunamadi.")
+            print(f"Kullanici {user_id}: Hata: Belirtilen anahtar Redis veritabaninda bulunamadi.")
+        time.sleep(1)
 
 def main():
-    redis_host = 'api.rsm-prod-cloud.vodafone.loca;'
+    redis_host = 'api.rsm-prod-cloud.vodafone.local'
     redis_port = 6379
     redis_auth_pass = 'Redis1234'
-    redis_client = redis.StrictRedis(host=redis_host, port=redis_port, auth=redis_auth_pass)
+    redis_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_auth_pass)
 
     user_count = 100
     threads = []
